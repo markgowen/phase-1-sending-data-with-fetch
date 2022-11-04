bf7603560c028d0c0434f95589b30cd92c8cd524
Mark Gowen <mark.gowen78@gmail.com>
2022-11-04 12:04:10 -0700

    commit tree ffd1f280bfc1eb7a18175a81ebdc5b1c2a858801

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName, email) {
+    return fetch(('http://localhost:3000/users'), {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({name: userName, email: email})
+         })
+    .then((response) => {
+        return response.json();
+    })
+    .then((object) => {
+        document.body.innerHTML = object["id"]
+    })
+    .catch((err) => {
+        document.body.innerHTML = err.message
+    })
+
+}
+
+submitData()
